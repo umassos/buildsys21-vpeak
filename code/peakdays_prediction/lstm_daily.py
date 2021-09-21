3b5fde6e479a6da34d81e5123cc2cd4bc6ec6a37
NikkO <nikko_romanot@hotmail.com>
2021-09-21 13:55:37 -0400

    fixed params

--- code/peakdays_prediction/lstm_daily.py
+++ code/peakdays_prediction/lstm_daily.py
@@ -13,16 +13,9 @@ from keras.wrappers.scikit_learn import KerasRegressor
 import math
 import matplotlib.pyplot as plt 
 
-def create_daily_lstm():
-    breg = 0.1
-    dropout = 0.2 
-    lr = 0.001 
-    recurrent = 'hard_sigmoid'
-    RS = False
-    f_bias = 0.1
-    bs = 0
+def create_daily_lstm(breg = 0.1, dropout = 0.2 , lr = 0.001, recurrent = 'hard_sigmoid', RS = False, f_bias = 0.1, bs = 0, stateful=True):
     model = Sequential()
-    model.add(LSTM(32,input_shape=(X_train.shape[1], X_train.shape[2]),kernel_initializer='glorot_uniform', bias_regularizer = regularizers.l2(breg),return_sequences=True,recurrent_activation = recurrent , return_state = RS,unit_forget_bias = f_bias, use_bias = bs))
+    model.add(LSTM(32,input_shape=(X_train.shape[1], X_train.shape[2]),kernel_initializer='glorot_uniform', bias_regularizer = regularizers.l2(breg),return_sequences=True,recurrent_activation = recurrent , return_state = RS,unit_forget_bias = f_bias, use_bias = bs, stateful=stateful))
     model.add(LSTM(16, dropout = dropout, return_sequences = False,recurrent_activation = recurrent ,return_state = RS,unit_forget_bias = f_bias, use_bias = bs))
     model.add(Dense(1))
 
